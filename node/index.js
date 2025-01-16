4e4cc42d8bcc3c160911d490257b7d64ac3bda90
omarlengliz <41199482+omarlengliz@users.noreply.github.com>
2025-01-16 16:59:00 +0100

    Update index.js

--- node/index.js
+++ node/index.js
@@ -27,8 +27,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}!`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}!`);
 });
 
 app.use("/api/user", userRoutes);
